d16daadde63413e92af6384168aeba611dca7017
Sunny Luk <swisswing@gmail.com>
2016-08-24 21:33:43 +0800

    agasfa

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,14 +1,15 @@
-class Food
+class Recipe
+	include HTTParty
 
-		FOOD2FORK_KEY = '67de6b1f2b65fbe408f13e1da985ad42'
-		base_uri = 'http://food2fork.com/api/search'
-		default_params key: FOOD2FORK_KEY
+	ENV['FOOD2FORK_KEY'] = '67de6b1f2b65fbe408f13e1da985ad42'
+		base_uri 'http://food2fork.com/api'
+		default_params key: ENV['FOOD2FORK_KEY']
 		format :json
 
 		def self.for term
-		get("", q: { q: term})["recipes"]
+
+		get("/search", query: { q: term})["recipes"]
 
 		end
-
 
 end
