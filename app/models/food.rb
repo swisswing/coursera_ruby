ed9420cae2079938cb9666e0ffcf1ab77c1efd4e
Sunny Luk <swisswing@gmail.com>
2016-08-24 21:30:17 +0800

    dfasjhkfa

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,7 +3,7 @@ class Food
 		FOOD2FORK_KEY = '67de6b1f2b65fbe408f13e1da985ad42'
 		base_uri = 'http://food2fork.com/api/search'
 		default_params key: FOOD2FORK_KEY
-		format: json
+		format :json
 
 		def self.for term
 		get("", q: { q: term})["recipes"]
