4e6be156beffcc17cea05310fb608bd3a8375467
Sunny Luk <swisswing@gmail.com>
2016-08-24 20:53:59 +0800

    abc

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,8 @@
 class CoursesController < ApplicationController
   def index
   	@search_term = params[:looking_for] || 'jhu'
-  	@courses = Coursera.for(@search_term)
+  	@courses = Coursera.for(@search_term) || []
   	#Coursera.get("",{})[element]
+
   end
 end
